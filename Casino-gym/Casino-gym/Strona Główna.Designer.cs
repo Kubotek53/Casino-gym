91a5cfb4b9e32a31354e10591ca8eccae9aab190
xNeon333 <adrianwieczorek333@gmail.com>
2025-10-08 10:46:15 +0200

    Pierwsze próby
    cos

---
+++ Casino-gym/Casino-gym/Strona Główna.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Casino_gym
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Logowanie2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Logowanie2
+            // 
+            this.Logowanie2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Logowanie2.Location = new System.Drawing.Point(0, 0);
+            this.Logowanie2.Name = "Logowanie2";
+            this.Logowanie2.Size = new System.Drawing.Size(75, 23);
+            this.Logowanie2.TabIndex = 0;
+            this.Logowanie2.Text = "Logowanie2";
+            this.Logowanie2.UseVisualStyleBackColor = false;
+            this.Logowanie2.Click += new System.EventHandler(this.Logowanie2_Click);
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(502, 417);
+            this.Controls.Add(this.Logowanie2);
+            this.Name = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Logowanie;
+        private System.Windows.Forms.Button Logowanie2;
+    }
+}
+
---
+++ Casino-gym/Casino-gym/Strona Główna.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Casino_gym
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void Logowanie_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Logowanie2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            this.Hide();
+        }
+    }
+}
